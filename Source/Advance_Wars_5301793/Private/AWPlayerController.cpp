1138749720869e81251785df9d73581401cfbbcd
thomasimo-cmd <5301793@studenti.unige.it>
2025-03-04 09:47:53 +0100

    downgraded version VS to 17.8.3 todo test functioning

--- Source/Advance_Wars_5301793/Private/AWPlayerController.cpp
+++ Source/Advance_Wars_5301793/Private/AWPlayerController.cpp
@@ -1,5 +1,66 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "AWPlayerController.h"
+#include "Components/InputComponent.h"
+#include "Kismet/GameplayStatics.h"  // Comment out for now
+#include "Engine/World.h" // Include the world header
 
+
+AAWPlayerController::AAWPlayerController()
+{
+    bShowMouseCursor = true;
+    bEnableClickEvents = true;
+    bIsMovingUnit = false;
+    bHasAttacked = false;
+}
+
+void AAWPlayerController::BeginPlay()
+{
+    Super::BeginPlay();
+
+    // Get the GameInstance
+    //GameInstance = Cast<UAWGameInstance>(UGameplayStatics::GetGameInstance(GetWorld())); // Comment out for now
+
+    if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer()))
+    {
+        Subsystem->AddMappingContext(InputContext, 0);
+    }
+}
+
+void AAWPlayerController::SetupInputComponent()
+{
+    Super::SetupInputComponent();
+
+    // Get the InputComponent
+    UInputComponent* InputComponent = GetInputComponent(); // Get the actual InputComponent
+
+    if (UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(InputComponent))
+    {
+        EnhancedInputComponent->BindAction(ClickAction, ETriggerEvent::Triggered, this, &AAWPlayerController::OnClick);
+    }
+}
+
+void AAWPlayerController::OnClick()
+{
+    /*
+    if (!GameInstance->IsPlayerTurn()) // Comment out for now
+    {
+        return;
+    }
+    */
+
+    FVector2D MousePosition;
+    GetMousePosition(MousePosition.X, MousePosition.Y);
+
+    FIntPoint GridCoordinates = GetGridCoordinatesFromScreenLocation(MousePosition);
+
+    // Convert grid coordinates to world location
+    //FVector WorldLocation = GameInstance->GetGameField()->GetTileWorldLocation(GridCoordinates.X, GridCoordinates.Y); // Comment out for now
+
+    // Line trace to find the clicked tile
+    FHitResult HitResult;
+    // Implement the line trace here, similar to how it's done in other parts of your code
+    // ...
+}
+
+// ... (rest of the method implementations)
--- Source/Advance_Wars_5301793/Public/AWPlayerController.h
+++ Source/Advance_Wars_5301793/Public/AWPlayerController.h
@@ -4,14 +4,66 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/PlayerController.h"
-#include "AWPlayerController.generated.h"
+//#include "AWGameInstance.h"  // Comment out GameInstance header for now
+#include "InputActionValue.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "AWPlayerController.generated.h" 
 
-/**
- * 
- */
 UCLASS()
 class ADVANCE_WARS_5301793_API AAWPlayerController : public APlayerController
 {
-	GENERATED_BODY()
-	
+    GENERATED_BODY()
+
+public:
+    AAWPlayerController();
+
+    UPROPERTY(EditAnywhere, Category = Input)
+    UInputMappingContext* InputContext;
+
+    UPROPERTY(EditAnywhere, Category = Input)
+    UInputAction* ClickAction;
+
+    UFUNCTION()
+    void OnClick();
+
+    // Remove RightClickAction
+
+protected:
+    virtual void BeginPlay() override;
+    virtual void SetupInputComponent() override;
+
+private:
+    UPROPERTY()
+    class AHumanPlayer* SelectedUnit;
+
+    UPROPERTY()
+    class ATile* DestinationTile;
+
+    UPROPERTY()
+    class ATile* StartingTile;
+
+    //class UAWGameInstance* GameInstance;  // Comment out GameInstance variable
+
+    bool bIsMovingUnit;
+
+    bool bHasAttacked;
+
+    FIntPoint GetGridCoordinatesFromScreenLocation(const FVector2D& ScreenLocation);
+
+    void SelectUnit(class AHumanPlayer* Unit);
+
+    void MoveUnit(class ATile* Tile);
+
+    void AttackUnit(class AHumanPlayer* Unit);
+
+    void DeselectUnit();
+
+    void ShowPossibleMovements();
+
+    void ClearPossibleMovements();
+
+    void ShowPossibleAttacks();
+
+    void ClearPossibleAttacks();
 };
