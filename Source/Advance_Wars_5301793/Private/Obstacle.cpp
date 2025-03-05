b5e93058fd54c4b7c824dc7a39a53511b44b8eb3
thomasimo-cmd <5301793@studenti.unige.it>
2025-03-05 10:41:26 +0100

    added obstacles

--- Source/Advance_Wars_5301793/Private/Obstacle.cpp
+++ Source/Advance_Wars_5301793/Private/Obstacle.cpp
@@ -1,27 +1,50 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "Obstacle.h"
 
 // Sets default values
 AObstacle::AObstacle()
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+    // Set this actor to call Tick() every frame. You can turn this off to improve performance if you don't need it.
+    PrimaryActorTick.bCanEverTick = true;
 
+    // Create the Static Mesh Component
+    ObstacleMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("ObstacleMesh"));
+    RootComponent = ObstacleMesh;
 }
 
 // Called when the game starts or when spawned
 void AObstacle::BeginPlay()
 {
-	Super::BeginPlay();
-	
+    Super::BeginPlay();
 }
 
 // Called every frame
 void AObstacle::Tick(float DeltaTime)
 {
-	Super::Tick(DeltaTime);
-
+    Super::Tick(DeltaTime);
 }
 
+// Function to set the obstacle type
+void AObstacle::SetObstacleType(EObstacleType NewType)
+{
+    ObstacleType = NewType;
+
+    // Load and set the appropriate mesh based on the obstacle type
+    if (ObstacleType == EObstacleType::Mountain)
+    {
+        // Load and set the mountain mesh
+        static ConstructorHelpers::FObjectFinder<UStaticMesh> MountainMeshAsset(TEXT("StaticMesh'/Game/Meshes/Mountain.Mountain'"));
+        if (MountainMeshAsset.Succeeded())
+        {
+            ObstacleMesh->SetStaticMesh(MountainMeshAsset.Object);
+        }
+    }
+    else if (ObstacleType == EObstacleType::Tree)
+    {
+        // Load and set the tree mesh
+        static ConstructorHelpers::FObjectFinder<UStaticMesh> TreeMeshAsset(TEXT("StaticMesh'/Game/Meshes/Tree.Tree'"));
+        if (TreeMeshAsset.Succeeded())
+        {
+            ObstacleMesh->SetStaticMesh(TreeMeshAsset.Object);
+        }
+    }
+}
--- Source/Advance_Wars_5301793/Public/Obstacle.h
+++ Source/Advance_Wars_5301793/Public/Obstacle.h
@@ -1,26 +1,45 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
 #include "Obstacle.generated.h"
 
+// Enum to define the different types of obstacles
+// Define the EObstacleType enum
+UENUM(BlueprintType)
+enum class EObstacleType : uint8
+{
+    Mountain,
+    Tree
+    // Add more types as needed (e.g., Water, Rock)
+};
+
 UCLASS()
 class ADVANCE_WARS_5301793_API AObstacle : public AActor
 {
-	GENERATED_BODY()
-	
-public:	
-	// Sets default values for this actor's properties
-	AObstacle();
+    GENERATED_BODY()
+
+public:
+    // Sets default values for this actor's properties
+    AObstacle();
+
+    // Obstacle Type (Mountain, Tree, etc.)
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Obstacle")
+    TEnumAsByte<EObstacleType> ObstacleType;
+
+    // Function to set the obstacle type
+    UFUNCTION(BlueprintCallable, Category = "Obstacle")
+    void SetObstacleType(EObstacleType NewType);
 
 protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
+    // Called when the game starts or when spawned
+    virtual void BeginPlay() override;
 
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
+public:
+    // Called every frame
+    virtual void Tick(float DeltaTime) override;
 
+    // Static Mesh Component for the obstacle's visual representation
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+    UStaticMeshComponent* ObstacleMesh;
 };
